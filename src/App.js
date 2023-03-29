a38c91e952d7d0ec0869c05b156ecb9793667c02
Abhishek Singhal <86764280+Abhimrt@users.noreply.github.com>
2023-03-29 19:24:04 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Cards from "./Components/Cards";
 import "./scss/main.css";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import MainVid from "./Components/Video/Main";
 import MainVoi from "./Components/Audio/Main";
 import MainMsg from "./Components/Message/Main";
@@ -9,7 +9,7 @@ function App() {
   return (
       <>
       <nav>
-        <img src={require("./images/black.png")} alt="" />
+          <Link to={"/"}><img src={require("./images/black.png")} alt="" /></Link>
       </nav>
       <Routes>
         <Route path="/" element={<Cards/>} />
@@ -17,7 +17,7 @@ function App() {
         <Route path="/message" element={<MainMsg/>} />
         <Route path="/voice" element={<MainVoi/>} />
       </Routes>
-
+    <div className="footer">Developed by <a href="https://abhimrt.github.io/Portfolio/">Abhishek Singhal</a> </div>
         
       </>
   );
